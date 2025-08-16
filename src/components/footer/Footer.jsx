e3f845e808e86efd3dee9f1730f9d0dfdc4aa289
broto1234 <satyo97du@gmail.com>
2025-08-16 13:17:39 +0200

    footer component updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import FooterLinks from './FooterLinks';
 
 export default function Footer() {
   return (
-    <footer className="pt-10 bg-[linear-gradient(to_bottom,_var(--color-bg01)_70%,_#ffffff_70%)]">
+    <footer className="pt-10 ">
       <FooterLinks />
       {/* <Dms /> */}
       <p className="bg-primary text-center text-sm text-white py-2">Coded By <span className="font-bold">Satyobroto Talukder</span> &copy; 2025</p>
