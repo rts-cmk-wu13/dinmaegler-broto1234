bf1caa499c2471810de0736002e4163194eab556
broto1234 <satyo97du@gmail.com>
2025-09-09 09:16:51 +0200

    updated agentInfoCard.

--- src/components/AgentInfoCard.jsx
+++ src/components/AgentInfoCard.jsx
@@ -7,7 +7,7 @@ const AgentInfoCard = ({agentData}) => {
   //  console.log(agentData);
 
   return (
-    <div className="w-[15rem] sm:w-[25rem] lg:w-[28rem] sm:grid grid-cols-2 md:gap-8 items-center justify-between">
+    <div className="sm:w-[25rem] lg:w-[28rem] sm:grid grid-cols-2 md:gap-8 items-center justify-between">
       <div className="w-[12rem] relative">
         <img src={agentData?.image?.url} alt={agentData?.name} />
         <SocialMedia />
--- src/pages/DetailAgent.jsx
+++ src/pages/DetailAgent.jsx
@@ -12,7 +12,7 @@ const DetailAgent = () => {
     <section>
       <Banner bannerText="Kontakt en medarbejder" />
       <div className="container my-4 sm:my-16 sm:grid grid-cols-3 gap-4">
-        <div className="sm:col-span-2 w-[16rem] sm:w-[25rem] lg:w-[28rem] p-4 border-1 border-borderLine">
+        <div className="sm:col-span-2 sm:w-[25rem] lg:w-[28rem] p-4 border-1 border-borderLine">
           <AgentInfoCard agentData={detailAgent} />
           <div className="my-4">
             <p className="font-semibold -mb-3">Om {detailAgent.name}</p>
